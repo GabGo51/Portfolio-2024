12c3d9bdd89e69a158f87f4862e6c1356e45cce2
GabrielGosselin <snow.gabo51@gmail.com>
2024-07-22 17:21:26 -0400

    updated contact

--- portfolio2024/src/components/Contact/Contact.jsx
+++ portfolio2024/src/components/Contact/Contact.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
-import "./Contact.css"
+import React, { useContext } from "react";
+import "./Contact.css";
+import { MouseContext } from "../../context/mouseContext";
 
 const Contact = () => {
+  const { cursorChangeHandler } = useContext(MouseContext);
   return (
-    <div id='connect' className='contact section'>
-
+    <div id="connect" className="contact section">
       <h3>Let's Connect</h3>
-      <div>
-        <h4> Email</h4>
-        <p>gosselin.gabriel@outlook.com</p>
-      </div>
-      <div>
-        <h4> Phone Number</h4>
-        <p>450 712 7842</p>
+      <div className="cards-container">
+        <a
+          href="mailto:gosselin.gabriel@outlook.com"
+          className="card"
+          onMouseEnter={() => cursorChangeHandler("header")}
+          onMouseLeave={() => cursorChangeHandler("")}
+        >
+          <div>
+            <h4>Email</h4>
+            <p>gosselin.gabriel@outlook.com</p>
+          </div>
+        </a>
+        <a
+          href="tel:+14507127842"
+          className="card"
+          onMouseEnter={() => cursorChangeHandler("header")}
+          onMouseLeave={() => cursorChangeHandler("")}
+        >
+          <div>
+            <h4>Phone Number</h4>
+            <p>450 712 7842</p>
+          </div>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
